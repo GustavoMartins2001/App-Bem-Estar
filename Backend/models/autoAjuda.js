6bbc5c549dcba3177e183bdcaa656be9e960656e
Gustavo Martins <gustavoms050@gmail.com>
2025-10-15 14:13:40 -0300

    deletado arquivos nao usados

--- Backend/models/autoAjuda.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-const autoAjuda = sequelize.define('AutoAjuda', {
-descricao: DataTypes.STRING,
-status: DataTypes.ENUM('ativo', 'avaliada'),
-link: DataTypes.STRING,
-created_at: DataTypes.DATE,
-});
-return autoAjuda;
-};
