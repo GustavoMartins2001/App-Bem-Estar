f78713087cc4258154fd1448cde61423ea779066
Beatriz Banaki <112290703+BeatrizBanaki@users.noreply.github.com>
2025-10-16 16:12:49 -0300

    Atualiza server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,26 +1,40 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const sequelize = require('./src/config/database');
+
 const authRoutes = require('./src/routes/auth.routes');
-const User = require('./src/models/User'); 
+const metaRoutes = require('./src/routes/meta.routes');
+const autoavaliacaoRoutes = require('./src/routes/autoavaliacao.routes');
+const relatorioRoutes = require('./src/routes/relatorio.routes');
+const conteudoRoutes = require('./src/routes/conteudo.routes');
+const suporteRoutes = require('./src/routes/suporte.routes');
+
+const User = require('./src/models/User');
+const Meta = require('./src/models/Meta');
+const Autoavaliacao = require('./src/models/Autoavaliacao');
+const Relatorio = require('./src/models/Relatorio'); 
+const Conteudo = require('./src/models/conteudo');
+const Suporte = require('./src/models/Suporte');
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
-app.use(cors({
-  // Permite que o front-end (http://localhost:5173, por exemplo) se conecte
-  origin: 'http://localhost:5173', 
-})); 
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/metas', metaRoutes);
+app.use('/api/autoavaliacoes', autoavaliacaoRoutes);
+app.use('/api/relatorios', relatorioRoutes); 
+app.use('/api/conteudos', conteudoRoutes);
+app.use('/api/suportes', suporteRoutes);
 
 async function connectToDatabase() {
   try {
     await sequelize.authenticate();
     console.log('✅ Conexão com o Banco de Dados estabelecida com sucesso.');
-    
+
     await sequelize.sync();
     console.log('✅ Modelos sincronizados com o Banco de Dados.');
 
@@ -29,7 +43,7 @@ async function connectToDatabase() {
     });
   } catch (error) {
     console.error('❌ Não foi possível conectar ao Banco de Dados:', error.message);
-    process.exit(1); 
+    process.exit(1);
   }
 }
 
